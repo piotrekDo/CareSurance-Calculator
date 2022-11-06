be2ea0c4a6b782ee4410b9e08bae771d2a031f73
Bartosz Mróz <bartosz.mroz@ecom.software>
2022-11-06 11:28:27 +0100

    Wrapper + date-fns

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -12,6 +12,7 @@ import {
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataContext } from "../App";
+import { Wrapper } from "./Wrapper";
 
 export const StartPage = () => {
   const navigate = useNavigate();
@@ -59,54 +60,48 @@ export const StartPage = () => {
   };
 
   return (
-    <Center bg="#ccc" h="100vh" flexDirection={"column"}>
-      <Heading as={"h2"} size="lg">
-        My ubezpieczamy, Ty jesteś bezpieczny
-      </Heading>
-      <Stack direction={"column"} bg="white" p={16}>
-        <FormControl
-          isInvalid={
-            !!context.basicData.registrationNumber &&
-            !isRegistrationNumberValid()
-          }
-        >
-          <FormLabel>Numer rejestracyjny</FormLabel>
-          <Input
-            type="text"
-            value={context.basicData.registrationNumber}
-            onChange={onRegistrationNumberChanged}
-          />
-          <FormErrorMessage>Niewłaściwy numer rejestracyjny</FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={!!context.basicData.peselNumber && !isPeselNumberValid()}
-        >
-          <FormLabel>Numer PESEL właściciela pojazdu</FormLabel>
-          <Input
-            type="text"
-            value={context.basicData.peselNumber}
-            onChange={onPeselNumberChanged}
-          />
-          <FormErrorMessage>Niewłaściwy numer PESEL</FormErrorMessage>
-        </FormControl>
-        <Checkbox
-          onChange={onPrivacyPolicyConsentChanged}
-          isInvalid={!context.basicData.privacyPolicyConsent}
-        >
-          Akceptuję politykę prywatności
-        </Checkbox>
-        <Button
-          colorScheme="red"
-          disabled={
-            !isRegistrationNumberValid() ||
-            !isPeselNumberValid() ||
-            !context.basicData.privacyPolicyConsent
-          }
-          onClick={() => navigate("/details-form")}
-        >
-          Sprawdź cenę
-        </Button>
-      </Stack>
-    </Center>
+    <Wrapper heading="My ubezpieczamy, Ty jesteś bezpieczny">
+      <FormControl
+        isInvalid={
+          !!context.basicData.registrationNumber && !isRegistrationNumberValid()
+        }
+      >
+        <FormLabel>Numer rejestracyjny</FormLabel>
+        <Input
+          type="text"
+          value={context.basicData.registrationNumber}
+          onChange={onRegistrationNumberChanged}
+        />
+        <FormErrorMessage>Niewłaściwy numer rejestracyjny</FormErrorMessage>
+      </FormControl>
+      <FormControl
+        isInvalid={!!context.basicData.peselNumber && !isPeselNumberValid()}
+      >
+        <FormLabel>Numer PESEL właściciela pojazdu</FormLabel>
+        <Input
+          type="text"
+          value={context.basicData.peselNumber}
+          onChange={onPeselNumberChanged}
+        />
+        <FormErrorMessage>Niewłaściwy numer PESEL</FormErrorMessage>
+      </FormControl>
+      <Checkbox
+        onChange={onPrivacyPolicyConsentChanged}
+        isInvalid={!context.basicData.privacyPolicyConsent}
+      >
+        Akceptuję politykę prywatności
+      </Checkbox>
+      <Button
+        colorScheme="red"
+        disabled={
+          !isRegistrationNumberValid() ||
+          !isPeselNumberValid() ||
+          !context.basicData.privacyPolicyConsent
+        }
+        onClick={() => navigate("/details-form")}
+      >
+        Sprawdź cenę
+      </Button>
+    </Wrapper>
   );
 };
