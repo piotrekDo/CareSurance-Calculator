243f4d0a113d36eddd4f62a4a0731c01874df99d
Bartosz Mróz <bartosz.mroz@ecom.software>
2022-11-06 11:28:27 +0100

    Wrapper + date-fns

--- src/components/DetailsPage.tsx
+++ src/components/DetailsPage.tsx
@@ -1,8 +1,31 @@
 import { useContext } from "react";
+import {
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 import { DataContext } from "../App";
+import { Wrapper } from "./Wrapper";
+import { format } from "date-fns";
 
 export const DetailsPage = () => {
   const context = useContext(DataContext);
 
-  return <div>Szczegóły: {context.basicData.registrationNumber}</div>;
+  return (
+    <Wrapper>
+      <Heading as="h2" size="lg">
+        Dane ubezpieczającego
+      </Heading>
+      <FormControl>
+        <FormLabel>Data rozpoczęcia ubezpieczenia</FormLabel>
+        <Input
+          type="date"
+          placeholder="Wybierz datę"
+          min={format(new Date(), "yyyy-MM-dd")}
+        />
+      </FormControl>
+    </Wrapper>
+  );
 };
